5a8144e98a8f48a26764ea81e5cf9d5ae4458c7e
Kleber Vales <klebervales.dev@gmail.com>
2026-01-07 14:18:49 -0300

    feat: create PostRepository

---
+++ src/main/java/com/example/blog/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.example.blog.repository;
+
+import com.example.blog.domain.post.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findByAuthorUsername(String username);
+}
+
